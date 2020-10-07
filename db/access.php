ecf337880f4e3114582cacd316d885ee1a4dd61a
anayinfowind <anay.infowind@gmail.com>
2020-10-07 17:04:34 +0530

    Update access.php

--- db/access.php
+++ db/access.php
@@ -24,17 +24,14 @@
  */
 defined('MOODLE_INTERNAL') || die();
 $capabilities = array(
- 
     'block/simplehtml:myaddinstance' => array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
             'user' => CAP_ALLOW
         ),
- 
         'clonepermissionsfrom' => 'moodle/my:manageblocks'
     ),
- 
     'block/simplehtml:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
  
@@ -44,7 +41,6 @@ $capabilities = array(
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
         ),
- 
         'clonepermissionsfrom' => 'moodle/site:manageblocks'
     ),
 );

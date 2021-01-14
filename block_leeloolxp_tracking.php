c57f755fd430be010c4d5c4aa3d836b27c831353
Anay <anay.infowind@gmail.com>
2021-01-14 16:05:56 +0530

    Change User status check by LLT

--- block_leeloolxp_tracking.php
+++ block_leeloolxp_tracking.php
@@ -82,7 +82,7 @@ class block_leeloolxp_tracking extends block_base {
             return true;
         }
         $useremail = $USER->email;
-        $url = $teamniourl . '/admin/sync_moodle_course/check_user_status_by_email/' . $useremail;
+        $url = $teamniourl . '/admin/sync_moodle_course/check_user_llt_status_by_email/' . $useremail;
         $curl = new curl;
         $options = array(
             'CURLOPT_RETURNTRANSFER' => true,
